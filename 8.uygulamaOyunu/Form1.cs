88dcaefa3f4f6c493c75faff70608b4a5e36ee09
necati148 <necati148@RPC11>
2024-03-13 13:16:57 +0300

    Proje dosyası ekle.

---
+++ 8.uygulamaOyunu/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8.uygulamaOyunu
+{
+    public partial class Form1 : Form
+    {
+        byte[,] dizi = new byte[4, 4];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYeni_Click(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            int satirRastgele = rastgele.Next(4);
+            int sutunRastgele = rastgele.Next(4);
+            dizi[satirRastgele, sutunRastgele] = 1;
+            for (byte satir = 0; satir < 4; satir++)
+            {
+                for (byte sutun = 0; sutun < 4; sutun++)
+                {
+                    PictureBox kutu = this.Controls.Find("p" + satir + sutun, true)[0] as PictureBox;
+                    kutu.BackColor = SystemColors.Control; 
+                }
+            }
+
+
+        }
+
+        private void btnGönder_Click(object sender, EventArgs e)
+        {
+            byte satir = byte.Parse(txtSatir.Text);
+            byte sutun = byte.Parse(txtSütün.Text);
+            PictureBox kutu = this.Controls.Find("p" + satir + sutun, true)[0] as PictureBox;
+            byte durum = dizi[satir, sutun];
+            if (durum == 0)
+            { kutu.BackColor = Color.Green; }
+            else
+            { kutu.BackColor = Color.Red; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            int satirRastgele = rastgele.Next(4);
+            int sutunRastgele = rastgele.Next(4);
+            dizi[satirRastgele, sutunRastgele] = 1;
+
+        }
+    }
+}
+
